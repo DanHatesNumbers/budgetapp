cb749663f532ec64e8d4829d2423769de4b69e3c
Daniel Wright <danwright91@gmail.com>
2016-03-06 19:27:20 +0000

    Users can now log in and log out. The logout landing page is ugly, but that can be replaced with a redirect to the landing page

--- src/budgetapp/budgetapp/settings.py
+++ src/budgetapp/budgetapp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'budgetapp.website',
+    'bootstrap3',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -56,7 +57,9 @@ ROOT_URLCONF = 'budgetapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "website", "templates"),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
