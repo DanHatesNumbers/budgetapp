54e7ccf05cb2a60fd3e66a97201a4452a6c9a19d
Daniel Wright <danwright91@gmail.com>
2016-03-13 15:15:43 +0000

    Simplified code for updating one-off transactions

--- src/budgetapp/budgetapp/website/views.py
+++ src/budgetapp/budgetapp/website/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView, FormView
-from django.views.generic.edit import DeleteView
+from django.views.generic.edit import UpdateView, DeleteView
 from django.contrib.auth.models import User
 from django.http import Http404, HttpResponseRedirect
 from django.core.exceptions import PermissionDenied
@@ -42,34 +42,30 @@ class OneOffAddView(LoginRequiredMixin, FormView):
         newtransaction.save()
         return super(OneOffAddView, self).form_valid(form)
 
-class OneOffEditView(LoginRequiredMixin, FormView):
+class OneOffEditView(LoginRequiredMixin, UpdateView):
     template_name = 'oneoffedit.html'
     success_url = '/'
     form_class = forms.OneOffTransactionForm
+    model = models.OneOffTransaction
 
     def get(self, request, *args, **kwargs):
-        self.pk = kwargs.pop('pk', None)
+        self.user = request.user
         return super(OneOffEditView, self).get(self, request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        self.pk = kwargs.pop('pk', None)
+        self.user = request.user
         return super(OneOffEditView, self).post(self, request, *args, **kwargs)
 
-    def get_context_data(self, **kwargs):
-        context = super(OneOffEditView, self).get_context_data(**kwargs)
-        context['pk'] = self.pk
-        return context
-
-    def get_form(self, form_class):
-        try:
-            oneoff = models.OneOffTransaction.objects.get(id=self.pk)
-            return form_class(instance=oneoff, **self.get_form_kwargs())
-        except models.OneOffTransaction.DoesNotExist:
-            raise Http404("Transaction can not be found")
+    def get_object(self, queryset=None):
+        obj = super(OneOffEditView, self).get_object(queryset)
+        if obj.owner == self.user:
+            return obj
+        else:
+            raise PermissionDenied()
 
     def form_valid(self, form):
         editedoneoff = form.save(commit=False)
-        if models.OneOffTransaction.objects.get(id=self.pk).owner == self.request.user:
+        if self.get_object().owner == self.user:
             editedoneoff.save()
             return super(OneOffEditView, self).form_valid(form)
         else:
